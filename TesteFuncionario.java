4811ef87460b26bb1784ce08542559457c67a34c
Jhonny07 <jhonnyjacinto09@icloud.com>
2021-11-25 18:15:49 +0000

    Semana 16 Funcionario

---
+++ TesteFuncionario.java
@@ -0,0 +1,13 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        
+       Funcionario f = new Funcionario();
+       
+       f.setCargo("Auxiliar administrativo");
+       f.setSalario(1500);
+
+       System.out.println(f.getCargo());
+       System.out.println(f.getSalario());
+    }
+   }
+
